b77172796951515b42308c80afaf96991762858a
Engineer-Karan-Mahato <engineerkaranmahato@gmail.com>
2026-06-18 12:36:16 +0000

    Add Mini-SoC lab: counter, alu, mux, testbench, run script

---
+++ mux.v
@@ -0,0 +1,11 @@
+module mux2x1 ( 
+// 2-bit 2:1 multiplexer  
+		input  wire [1:0] in0,    
+		input  wire [1:0] in1,    
+		input  wire       sel,  
+		output wire [1:0] out 
+	); 
+
+	assign out = sel ? in1 : in0;  
+endmodule 
+
